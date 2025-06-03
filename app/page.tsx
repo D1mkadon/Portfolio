6169baab81bc6d5b2586dff910f4013782adc78c
Dmytro Tarasenko <dima0938157970@gmail.com>
2025-06-03 16:20:58 +0200

    test

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import Particles from "./particles";
+
+export default function Home() {
+  return (
+    <div className="flex bg-blue-[200] flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="my-16 animate-fade-in">
+      </nav>
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}  
+      />
+
+      <h1 className="mt-2 scroll-m-20 text-4xl font-bold tracking-tight py-3.5 px-0.5 z-15 text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        dimasik
+      </h1>
+
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="my-16 text-center animate-fade-in">
+        <h2 className="text-sm text-zinc-500 ">
+          I'm building{" "}
+          <Link
+            target="_blank"
+            href="https://unkey.dev"
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            unkey.dev
+          </Link> to solve API authentication and authorization for developers.
+        </h2>
+      </div>
+    </div>
+  );
+}
